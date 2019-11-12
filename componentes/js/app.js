f0624c512cc37993d5557537bbf833e2da4834ab
samuelkepler <57400752+samuelkepler@users.noreply.github.com>
2019-11-12 10:23:33 -0300

    Feito 1

---
+++ componentes/js/app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAvBbRKSZjnUvRQua5O7VcdE0WsRkWgdtg",
+    authDomain: "to-help-you-be5fb.firebaseapp.com",
+    databaseURL: "https://to-help-you-be5fb.firebaseio.com",
+    projectId: "to-help-you-be5fb",
+    storageBucket: "to-help-you-be5fb.appspot.com",
+    messagingSenderId: "23437693696",
+    appId: "1:23437693696:web:2774551cf9e13085a899e5",
+    measurementId: "G-H3C238L7ZN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
