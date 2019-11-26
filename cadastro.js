02f1ef89e24192f23972dfe7cdc88eb503c71e78
guilhermemartins1 <57631289+guilhermemartins1@users.noreply.github.com>
2019-11-25 21:32:14 -0300

    Create cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,14 @@
+function cadastro() {
+
+    let email = document.getElementById("Email").value;
+    let senha = document.getElementById("Senha").value;
+    let endereço = document.getElementById("Endereço").value;
+    let cidade = document.getElementById("Cidade").value;
+    let estado = document.getElementById("Estado").value;
+
+
+
+
+    let usuarios = [];
+    usuarios.push({email: email, senha:senha, endereco:endereço, cidade:cidade, estado:estado});
+
