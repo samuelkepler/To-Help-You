5e4df5a8ca0936add217d33934952425d670174c
guilhermemartins1 <57631289+guilhermemartins1@users.noreply.github.com>
2019-11-25 21:16:06 -0300

    Update homeFree.js

--- homeFree.js
+++ homeFree.js
@@ -1,13 +1,16 @@
-const searchBar = document.forms['search'].queryselector('input');
-searchBar.addEventListener('keyup', function(e){
-    const term = e.target.value.toLowerCase();
-    const videos = list.getElementsByTagName('body');
-    Array.from(books).forEach(function(video){
-        const title = video.firstElementChild.text.Content;
-        if (title.toLowerCase().indexOf(term) != -1) {
-            video.style.disply = 'block';
-        } else {
-            video.style.disply = 'none';
-        }
-    })
-})
+const list = document.querySelector('#cards');
+const forms = document.forms;
+
+const searchBar = forms['search-video'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+  const term = e.target.value.toLowerCase();
+  const videos = list.getElementsByTagName('p');
+  Array.from(videos).forEach((video) => {
+    const title = video.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(e.target.value) != -1){
+      video.style.display = 'block';
+    } else {
+      video.style.display = 'none';
+    }
+  });
+});
