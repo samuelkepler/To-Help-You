9c477abbdfc546f46d141860d9924a25ce7edb61
guilhermemartins1 <57631289+guilhermemartins1@users.noreply.github.com>
2019-11-29 00:01:07 -0300

    Update homeFree.js

--- componentes/js/homeFree.js
+++ componentes/js/homeFree.js
@@ -1,16 +1,38 @@
-const list = document.querySelector('#cards');
-const forms = document.forms;
+// duas ferramentas de busca
 
-const searchBar = forms['search-video'].querySelector('input');
-searchBar.addEventListener('keyup', (e) => {
-  const term = e.target.value.toLowerCase();
-  const videos = list.getElementsByTagName('p');
-  Array.from(videos).forEach((video) => {
-    const title = video.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(e.target.value) != -1){
-      video.style.display = 'block';
-    } else {
-      video.style.display = 'none';
+// const list = document.querySelector('#cards');
+// const forms = document.forms;
+
+// const searchBar = forms['search-video'].querySelector('input');
+// searchBar.addEventListener('keyup', (e) => {
+//   const term = e.target.value.toLowerCase();
+//   const videos = list.getElementsByTagName('p');
+//   Array.from(videos).forEach((video) => {
+//     const title = video.firstElementChild.textContent;
+//     if(title.toLowerCase().indexOf(e.target.value) != -1){
+//       video.style.display = 'block';
+//     } else {
+//       video.style.display = 'none';
+//     }
+//   });
+// });
+
+$("#search-video").keyup(function() {
+  let text = $(this).val();
+  $(".boxes div.card").each(function() {
+    if ($(this).text().indexOf(text)<0) {
+      $(this).css("dispay","none");
     }
-  });
-});
+    else if ($(this).text().indexOf(text)>0) {
+      $(this).css("display","block").addClass("filterAdjust2");
+      $(".boxes div#cards").addClass("filterAdjust");
+    }
+    else if (text=="") {
+      $(this).css("disply", "block").removeClass("filterAdjust2");
+      $(".boxes dic#cards").removeClass("filterAdjusted");
+
+    }
+  })  
+})
+
+
